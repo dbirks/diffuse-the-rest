f09e28b9bdecbb756c12d9f177a6b235f0d712d8
Mishig Davaadorj <dmishig@gmail.com>
2022-08-24 17:15:15 +0200

    prepare build

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true
-	}),
+	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		paths: {
+			base: dev ? '' : '/staticspaceiframe/huggingface/diffuse-the-rest/build',
+		},
+		adapter: adapter(),
+		prerender:{
+			default: true,
+		}
 	}
 };
 
