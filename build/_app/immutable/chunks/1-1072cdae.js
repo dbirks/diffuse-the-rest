0f8aece634effb0dbc0cf39e93b337bfccc53e0d
Mishig Davaadorj <dmishig@gmail.com>
2022-08-26 16:17:57 +0200

    new build

--- build/_app/immutable/chunks/1-1072cdae.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-9b93453d.js";import"./index-c0a82f06.js";import"./singletons-32a58e07.js";export{r as component};
---
+++ build/_app/immutable/chunks/1-4138e348.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-432ee809.js";import"./index-c0a82f06.js";import"./singletons-b93a376b.js";export{r as component};
