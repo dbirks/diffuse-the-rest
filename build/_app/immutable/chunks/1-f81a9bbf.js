18487d89b21b66d0abba929048722cb0b61f300a
David Birks <david@birks.dev>
2022-09-12 22:42:23 -0400

    new build

---
+++ build/_app/immutable/chunks/1-f81a9bbf.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-2ced7150.js";import"./index-a207c28c.js";import"./singletons-ed49354d.js";export{r as component};
---
+++ build/_app/immutable/chunks/2-37543285.js
@@ -0,0 +1,1 @@
+import{default as m}from"../components/pages/_page.svelte-9c924844.js";import"./index-a207c28c.js";export{m as component};
