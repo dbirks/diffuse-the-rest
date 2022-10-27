5dcfcf8be425c78f2c804da27e05728464877d12
Mishig Davaadorj <dmishig@gmail.com>
2022-10-27 15:30:44 +0200

    No more paths

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,6 @@ import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -14,9 +12,6 @@ const config = {
 	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
-		paths: {
-			base: dev ? '' : '/staticspaceiframe/huggingface-projects/diffuse-the-rest/build'
-		},
 		adapter: adapter(),
 		prerender: {
 			default: true
